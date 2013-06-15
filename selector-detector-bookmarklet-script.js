fb769146d1453f9c4599933dae1936924fa46ca1
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2013-06-15 18:24:04 -0400

    updates

--- selector-detector-bookmarklet-script.js
+++ selector-detector-bookmarklet-script.js
@@ -1,17 +1,21 @@
 (function() {
 
   var stylesheets = document.styleSheets,
-      ss,
-      ssLink,
+      stylesheetsTotal = stylesheets.length,
+      stylesheet,
+      stylesheetLink,
       stylesheetName,
       stylesheetRules,
-      totalRulesInStylesheet,
-      totalSelectorsInStylesheet;
+      totalInlineStylesheets = 0,
+      totalRulesInStylesheet = 0,
+      totalSelectorsInStylesheet = 0;
 
-  for (var i = 0; i < stylesheets.length; i++) {
+  for (var i = 0; i < stylesheetsTotal; i++) {
 
     stylesheet = stylesheets[i],
     stylesheetLink = stylesheet.href;
+
+    console.log(stylesheetLink);
 
     if (stylesheetLink !== null ) {
 
@@ -19,22 +23,32 @@
       stylesheetNameParsed = stylesheetName.replace(/[-]/g, ' ').replace(/.css/, ''),
       stylesheetRules = stylesheet.cssRules;
 
-      // check that rules exists (will break on empty stylesheet)
-      totalRulesInStylesheet = stylesheetRules.length,
-      totalSelectorsInStylesheet = 0;
+      if (stylesheetRules !== null ) {
 
-      for (var j = 0; j < totalRulesInStylesheet; j++) {
+        // check that rules exists (will break on empty stylesheet)
+        totalRulesInStylesheet = stylesheetRules.length;
 
-        if (stylesheetRules[j].selectorText) {
-          totalSelectorsInStylesheet += stylesheetRules[j].selectorText.split(',').length;
+        for (var j = 0; j < totalRulesInStylesheet; j++) {
+
+          if (stylesheetRules[j].selectorText) {
+            totalSelectorsInStylesheet += stylesheetRules[j].selectorText.split(',').length;
+          }
+
         }
+
+        console.log( '[' + stylesheetNameParsed + '] has ' + totalRulesInStylesheet + ' rules and ' + totalSelectorsInStylesheet + ' selectors.');
 
       }
 
-      console.log( '[' + stylesheetNameParsed + '] has ' + totalRulesInStylesheet + ' rules and ' + totalSelectorsInStylesheet + ' selectors.');
+    } else {
+
+      console.log('foo');
 
     }
 
   }
+
+  console.log('Found ' + (stylesheetsTotal - totalInlineStylesheets) + ' linked stylesheets');
+  console.log('Found ' + totalInlineStylesheets + ' inline style blocks');
 
 }).call(this);
