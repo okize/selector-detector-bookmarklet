48f091bc09733110a4dd9c3eee5c64532b17a75d
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2013-06-17 09:30:36 -0400

    updates

--- selector-detector-bookmarklet-script.js
+++ selector-detector-bookmarklet-script.js
@@ -1,54 +1,82 @@
 (function() {
 
-  var stylesheets = document.styleSheets,
-      stylesheetsTotal = stylesheets.length,
-      stylesheet,
-      stylesheetLink,
-      stylesheetName,
-      stylesheetRules,
-      totalInlineStylesheets = 0,
-      totalRulesInStylesheet = 0,
-      totalSelectorsInStylesheet = 0;
+  var
+    stylesheets = document.styleSheets,
+    count = {
+      stylesheets: {
+        total: stylesheets.length,
+        inline: 0,
+        linked: 0
+      },
+      parsed: []
+    },
+    stylesheet,
+    stylesheetLink,
+    stylesheetName,
+    stylesheetRules,
+    ssObj;
 
-  for (var i = 0; i < stylesheetsTotal; i++) {
+  // returns count of selectors in ruleset
+  var getSelectorCount = function(selectors) {
+
+    if (selectors) {
+      return selectors.split(',').length;
+    }
+
+    return 0;
+
+  };
+
+  for (var i = 0; i < count.stylesheets.total; i++) {
+
+    ssObj = {
+      name: '',
+      rules: 0,
+      selectors: 0
+    };
 
     stylesheet = stylesheets[i],
     stylesheetLink = stylesheet.href;
 
-    console.log(stylesheetLink);
-
+    // is linked stylesheets
     if (stylesheetLink !== null ) {
 
-      stylesheetName = stylesheetLink.split('/').pop(),
-      stylesheetNameParsed = stylesheetName.replace(/[-]/g, ' ').replace(/.css/, ''),
+      count.stylesheets.linked++;
+
       stylesheetRules = stylesheet.cssRules;
 
-      if (stylesheetRules !== null ) {
+      ssObj.name = stylesheetLink.split('/').pop();
+      ssObj.rules = stylesheetRules.length;
 
-        // check that rules exists (will break on empty stylesheet)
-        totalRulesInStylesheet = stylesheetRules.length;
-
-        for (var j = 0; j < totalRulesInStylesheet; j++) {
-
-          if (stylesheetRules[j].selectorText) {
-            totalSelectorsInStylesheet += stylesheetRules[j].selectorText.split(',').length;
-          }
-
-        }
-
-        console.log( '[' + stylesheetNameParsed + '] has ' + totalRulesInStylesheet + ' rules and ' + totalSelectorsInStylesheet + ' selectors.');
-
+      for (var j = 0; j < ssObj.rules; j++) {
+        ssObj.selectors += getSelectorCount(stylesheetRules[j].selectorText);
       }
 
-    } else {
+      count.parsed.push(ssObj);
 
-      console.log('foo');
+    }
+
+    // is inline style block
+    else {
+
+      count.stylesheets.inline++;
+
+      stylesheetRules = stylesheet.cssRules;
+
+      ssObj.name = 'inline-style-block-' + count.stylesheets.inline;
+      ssObj.rules = stylesheetRules.length;
+
+      for (var k = 0; k < ssObj.rules; k++) {
+        ssObj.selectors += getSelectorCount(stylesheetRules[k].selectorText);
+      }
+
+      count.parsed.push(ssObj);
 
     }
 
   }
 
-  console.log('Found ' + (stylesheetsTotal - totalInlineStylesheets) + ' linked stylesheets');
-  console.log('Found ' + totalInlineStylesheets + ' inline style blocks');
+  console.dir(count);
+  // console.log( '[' + stylesheetNameParsed + '] has ' + totalRulesInStylesheet + ' rules and ' + totalSelectorsInStylesheet + ' selectors.');
 
 }).call(this);
