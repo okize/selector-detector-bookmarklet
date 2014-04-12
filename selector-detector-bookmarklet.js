b800c27881d1493566c61eb11795f8cdd378b533
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-04-12 07:00:00 -0400

    updates bookmarklet to correctly count media query selectors

--- selector-detector-bookmarklet.js
+++ selector-detector-bookmarklet.js
@@ -1,32 +1,51 @@
 (function(window, document) {
 
-  var
-    stylesheets = document.styleSheets,
-    count = {
+  var stylesheets = document.styleSheets;
+  var count = {
       stylesheets: {
         total: stylesheets.length,
         inline: 0,
         linked: 0
       },
       parsed: []
-    },
-    stylesheet,
-    stylesheetLink,
-
-    stylesheetRules;
+    };
+  var stylesheet;
+  var stylesheetLink;
+  var stylesheetRules;
 
   // returns count of selectors in ruleset
   var getSelectorCount = function (ruleset) {
 
-    var count = 0, selectors;
+    var count = 0;
+    var selectors;
+    var selectors_;
+    var rs;
 
     if (ruleset !== null) {
 
       for (var i = 0; i < ruleset.length; i++) {
-        selectors = ruleset[i].selectorText;
-        if (selectors) {
-          count += selectors.split(',').length;
+
+        rs = ruleset[i];
+
+        // normal css style rules
+        if (rs.type === 1) {
+          selectors = rs.selectorText;
+          if (selectors) {
+            count += selectors.split(',').length;
+          }
         }
+
+        // media query rules
+        else if (rs.type === 4) {
+
+          for (var j = 0; j < rs.cssRules.length; j++) {
+            selectors_ = rs.cssRules[j].selectorText;
+            if (selectors_) {
+              count += selectors_.split(',').length;
+            }
+          }
+        }
+
       }
 
     }
@@ -92,8 +111,11 @@
 
     } else {
 
-      // cross-domain
-      console.log('Stylesheets on cross domains can not be parsed:\n' + stylesheet.href);
+      // cross-domain requests won't work
+      if (stylesheetLink.indexOf(location.origin) !== 0) {
+        count.stylesheets.linked++;
+        console.log('Stylesheet could not be parsed because it is on a different domain:\n' + stylesheet.href + '\n');
+      }
 
     }
 
